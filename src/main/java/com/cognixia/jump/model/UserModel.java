8114827871931816869f6481298d1ff17366e51c
Austin Brooks <austin.williambrooks@gmail.com>
2021-07-20 12:35:24 -0800

    Added UserModel

---
+++ src/main/java/com/cognixia/jump/model/UserModel.java
@@ -0,0 +1,81 @@
+package com.cognixia.jump.model;
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserModel implements Serializable{
+	private static final long serialVersionUID = 1L;
+	public static enum Role{
+		ROLE_USER, ROLE_ADMIN
+	}
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(unique = true)
+	private String username;
+	@Column( nullable = false )
+	private String password;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private Role role;
+	public UserModel() {
+		this(-1L, "N/A","N/A", Role.ROLE_USER);
+	}
+	
+	public UserModel(Long id, String username, String password, Role role) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.role = role;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public Role getRole() {
+		return role;
+	}
+	
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", username=" + username + ", password=" + password
+				+ ", role=" + role + "]";
+	}
+}
