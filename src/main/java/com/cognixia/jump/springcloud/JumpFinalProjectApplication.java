fabed7dbd3fb522098793dc1fbe4c486247babc7
Austin Brooks <austin.williambrooks@gmail.com>
2021-07-20 12:15:16 -0800

    Initial commit

---
+++ src/main/java/com/cognixia/jump/springcloud/JumpFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.cognixia.jump.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JumpFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JumpFinalProjectApplication.class, args);
+	}
+
+}
