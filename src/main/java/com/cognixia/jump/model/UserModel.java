d7bef9abe2adbc6bad0c498ea126365b0459235d
Nikita Tran <nikiusa09@yahoo.com>
2021-07-21 08:17:27 -0400

    adjustments to models

--- src/main/java/com/cognixia/jump/model/UserModel.java
+++ src/main/java/com/cognixia/jump/model/UserModel.java
@@ -1,4 +1,5 @@
 package com.cognixia.jump.model;
+
 import java.io.Serializable;
 import java.util.List;
 
@@ -15,31 +16,35 @@ import javax.persistence.OneToMany;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
-public class UserModel implements Serializable{
+public class UserModel implements Serializable {
 	private static final long serialVersionUID = 1L;
-	public static enum Role{
+
+	public static enum Role {
 		ROLE_USER, ROLE_ADMIN
 	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	@Column(unique = true)
 	private String username;
-	@Column( nullable = false )
-	private String password;
 	
+	@Column(nullable = false)
+	private String password;
+
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private Role role;
-	
-	@OneToMany( mappedBy = "user", cascade = CascadeType.ALL )
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
 	@JsonManagedReference
 	private List<ReviewModel> reviews;
-	
+
 	public UserModel() {
-		this(-1L, "N/A","N/A", Role.ROLE_USER);
+		this(-1L, "N/A", "N/A", Role.ROLE_USER);
 	}
-	
+
 	public UserModel(Long id, String username, String password, Role role) {
 		super();
 		this.id = id;
@@ -47,46 +52,45 @@ public class UserModel implements Serializable{
 		this.password = password;
 		this.role = role;
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getUsername() {
 		return username;
 	}
-	
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
-	
+
 	public String getPassword() {
 		return password;
 	}
-	
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public Role getRole() {
 		return role;
 	}
-	
+
 	public void setRole(Role role) {
 		this.role = role;
 	}
-	
+
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
-	
+
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", username=" + username + ", password=" + password
-				+ ", role=" + role + "]";
+		return "User [id=" + id + ", username=" + username + ", password=" + password + ", role=" + role + "]";
 	}
 }
