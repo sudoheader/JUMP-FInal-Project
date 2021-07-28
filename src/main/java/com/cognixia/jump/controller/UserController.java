a2876673605e4ebe26c998470d8c9ddb9adac7fe
Nikita Tran <nikiusa09@yahoo.com>
2021-07-28 16:36:18 -0400

    fixed syntax error in user controller

--- src/main/java/com/cognixia/jump/controller/UserController.java
+++ src/main/java/com/cognixia/jump/controller/UserController.java
@@ -22,57 +22,49 @@ import com.cognixia.jump.repository.UserRepo;
 
 import io.swagger.annotations.ApiOperation;
 
-
 @RequestMapping("/api")
 @RestController
 public class UserController {
-	
+
 	@Autowired
 	UserRepo userRepo;
-	
+
 	@GetMapping("/users")
-  @ApiOperation(value = "Find all Users",
-	  notes = "Get all user names",
-	  response = User.class)
+	@ApiOperation(value = "Find all Users", notes = "Get all user names", response = User.class)
 	public ResponseEntity<List<User>> getAllUsers() {
 
 		List<User> list = userRepo.findAll();
-		
-		return ResponseEntity.status(HttpStatus.OK)
-							 .body(userRepo.findAll());
+
+		return ResponseEntity.status(HttpStatus.OK).body(userRepo.findAll());
 	}
-	
-	@GetMapping("/users/{user_id}")
-  @ApiOperation(value = "Find users by id",
+
+	@GetMapping("/users/{user_id")
+	@ApiOperation(value = "Find users by id",
 	  notes = "Get the user with a specific id",
 	  response = User.class)
 	public ResponseEntity<User> getUserById(@Valid @PathVariable("user_id") Long user_id) throws ResourceNotFoundException {
 		if(!userRepo.existsById(user_id)) {
 			throw new ResourceNotFoundException("User with id " + user_id + " not found");
+		}
 		
 	    User user = userRepo.findById(user_id).get();
 	    
 	    return ResponseEntity.status(HttpStatus.OK)
 	                        .body(user);
-		
-	}	
-	
+	}
+
 	@PostMapping("/users")
-   @ApiOperation(value = "Add a user",
-	  notes = "Initialize a new user",
-	  response = User.class)
+	@ApiOperation(value = "Add a user", notes = "Initialize a new user", response = User.class)
 
 	public ResponseEntity<User> addUser(@Valid @RequestBody User user) throws Exception {
-		
+
 		user.setId(-1L);
-		
-		user.newReview();		
-		
+
+		user.newReview();
+
 		User newUser = userRepo.save(user);
-		
-		return ResponseEntity.status(HttpStatus.CREATED)
-							 .body(newUser);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(newUser);
 	}
-	
-	
+
 }
