ce566044ccff5caf99990b98f3dc9b1fd8d07df8
Austin Brooks <austin.williambrooks@gmail.com>
2021-07-30 10:38:50 -0800

    Updated UserController to include all crud operations

--- src/main/java/com/cognixia/jump/controller/UserController.java
+++ src/main/java/com/cognixia/jump/controller/UserController.java
@@ -7,11 +7,15 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognixia.jump.exception.InvalidInputException;
@@ -25,11 +29,11 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/api")
 @RestController
 public class UserController {
-	
+								
 	@Autowired
 	UserRepo userRepo;
 	
-	@GetMapping("/users")
+	@GetMapping("/users")					
 	@ApiOperation(value = "Find all Users",
 	  notes = "Get all user names",
 	  response = User.class)
@@ -40,7 +44,8 @@ public class UserController {
 							 .body(list);
 	}
 	
-	@GetMapping("/users/{user_id}")
+	//Read
+	@GetMapping("/users/{user_id}")			
 	@ApiOperation(value = "Find users by id",
 	  notes = "Get the user with a specific id",
 	  response = User.class)
@@ -58,6 +63,7 @@ public class UserController {
 		}
 	}	
 	
+	//Create
 	@PostMapping("/users")
 	@ApiOperation(value = "Add a user",
 	  notes = "Initialize a new user",
@@ -73,6 +79,46 @@ public class UserController {
 		return ResponseEntity.status(201)
 							 .body(newUser);
 	}
+	
+	//Update
+	@CrossOrigin
+	@PutMapping("/update/users")
+	public @ResponseBody String updateUser(@RequestBody User updateUser) {
+		
+		// check if user exists, then update them
+		
+		Optional<User> found = userRepo.findById(updateUser.getId());
+		
+		if(found.isPresent()) {
+			userRepo.save(updateUser);
+			return "Saved: " + updateUser.toString();
+		}
+		else {
+			return "Could not update student, the id = " + updateUser.getId() + " doesn't exist";
+		}
+		
+	}
+	
+	//Delete
+	@DeleteMapping("/delete/users/{id}")
+	public ResponseEntity<String> deleteStudent(@PathVariable long id) {
+		
+		Optional<User> found = userRepo.findById(id);
+		
+		if(found.isPresent()) {
+			
+			userRepo.deleteById(id);
+			
+			return ResponseEntity.status(200).body("Deleted User with id = " + id);	
+		}
+		else {
+			return ResponseEntity.status(400)
+					.header("User id", id + "")
+					.body("User with id = " + id + " not found");
+		}
+			
+	}
+	
 	
 	
 }
