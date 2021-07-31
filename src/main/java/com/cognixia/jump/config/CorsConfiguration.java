78b126b08e6558d439b3b745459ddf5e3b49b70e
Raymond Nazaryan <sudoheader@users.noreply.github.com>
2021-07-31 02:22:51 -0700

    added cors configuration (works for now)

--- src/main/java/com/cognixia/jump/config/CorsConfiguration.java
+++ src/main/java/com/cognixia/jump/config/CorsConfiguration.java
@@ -8,11 +8,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
-	
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 
-		registry.addMapping("/*")
+		registry.addMapping("/api/**")
+				.allowedOrigins("http://localhost:3000")
 				.allowCredentials(true)
 				.allowedMethods("GET", "PUT", "POST", "HEAD", "DELETE", "OPTIONS");
 	}
