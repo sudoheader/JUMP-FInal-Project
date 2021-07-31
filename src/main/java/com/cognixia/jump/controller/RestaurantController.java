722a562639cbeba611e0ab3ed2d0db13b0e1fe2f
Nikita Tran <nikiusa09@yahoo.com>
2021-07-31 01:17:44 -0400

    mock testing successful CRUD api calls for restaurant controller done

--- src/main/java/com/cognixia/jump/controller/RestaurantController.java
+++ src/main/java/com/cognixia/jump/controller/RestaurantController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cognixia.jump.exception.InvalidInputException;
@@ -30,100 +31,90 @@ import io.swagger.annotations.ApiOperation;
 @RequestMapping("/api")
 @RestController
 public class RestaurantController {
-	
-	@Autowired
-	RestaurantRepo restaurantRepo;																
 
-	//CREATE
-	@PostMapping("/restaurants")
-	public ResponseEntity<Restaurant> addRestaurant(@Valid @RequestBody Restaurant restaurant) throws Exception {
-		restaurant.setId(-1L);
-		
-		Restaurant added = restaurantRepo.save(restaurant);
-		
-		return ResponseEntity.status(HttpStatus.OK)
-		 .body(added);
-	}
-	
-	//READ
+	@Autowired
+	RestaurantRepo restaurantRepo;
+
+	// READ
 	@GetMapping("/restaurants")
-	@ApiOperation(value = "Find all restaurants",
-	  notes = "Get all restaurant names",
-	  response = Restaurant.class)
+	@ApiOperation(value = "Find all restaurants", notes = "Get all restaurant names", response = Restaurant.class)
 	public ResponseEntity<List<Restaurant>> getAllRestaurants() {
 		List<Restaurant> list = restaurantRepo.findAll();
 
 		return ResponseEntity.status(HttpStatus.OK).body(list);
 	}
-	
-	//READ
+
+	// READ
 	@GetMapping("/restaurants/id/{restaurant_id}")
-	@ApiOperation(value = "Find restaurant by its id",
-	  notes = "Return the restaurant",
-	  response = Restaurant.class)
-	public ResponseEntity<Restaurant> getRestaurantById(@Valid @PathVariable("restaurant_id") Long restaurant_id) throws ResourceNotFoundException {
-		if(!restaurantRepo.existsById(restaurant_id)) {
+	@ApiOperation(value = "Find restaurant by its id", notes = "Return the restaurant", response = Restaurant.class)
+	public ResponseEntity<Restaurant> getRestaurantById(@Valid @PathVariable("restaurant_id") Long restaurant_id)
+			throws ResourceNotFoundException {
+		if (!restaurantRepo.existsById(restaurant_id)) {
 			throw new ResourceNotFoundException("Restaurant with id " + restaurant_id + " not found");
 		}
-		
-	    Restaurant restaurant = restaurantRepo.findById(restaurant_id).get();
-	    
-	    return ResponseEntity.status(HttpStatus.OK)
-	                        .body(restaurant);
+
+		Restaurant restaurant = restaurantRepo.findById(restaurant_id).get();
+
+		return ResponseEntity.status(HttpStatus.OK).body(restaurant);
 	}
-	
+
 	@GetMapping("/restaurants/name/{name}")
-	public ResponseEntity<Restaurant> getRestaurantByName(@Valid @PathVariable("name") String name) throws ResourceNotFoundException {
-		if(!restaurantRepo.existsByRestaurantName(name)) {
+	public ResponseEntity<Restaurant> getRestaurantByName(@Valid @PathVariable("name") String name)
+			throws ResourceNotFoundException {
+		if (!restaurantRepo.existsByRestaurantName(name)) {
 			throw new ResourceNotFoundException("Restaurant with name " + name + " not found");
 		}
-		
-	    Restaurant restaurant = restaurantRepo.findByRestaurantName(name).get();
-	    
-	    return ResponseEntity.status(HttpStatus.OK)
-	                        .body(restaurant);
+
+		Restaurant restaurant = restaurantRepo.findByRestaurantName(name).get();
+
+		return ResponseEntity.status(HttpStatus.OK).body(restaurant);
 	}
-	
-	//UPDATE
+
+	// CREATE
+	@PostMapping("/restaurants")
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Restaurant> addRestaurant(@Valid @RequestBody Restaurant restaurant) throws Exception {
+		restaurant.setId(-1L);
+
+		Restaurant added = restaurantRepo.save(restaurant);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(added);
+	}
+
+	// UPDATE
 	@PutMapping("/update/restaurants")
-	@ApiOperation(value = "Update a restaurant",
-			notes = "Restaurant to be updated",
-			response = Restaurant.class)
-	public  ResponseEntity<Restaurant> updateRestaurant(@Valid @RequestBody Restaurant restaurant) throws ResourceNotFoundException {
-		
+	@ApiOperation(value = "Update a restaurant", notes = "Restaurant to be updated", response = Restaurant.class)
+	public ResponseEntity<Restaurant> updateRestaurant(@Valid @RequestBody Restaurant restaurant)
+			throws ResourceNotFoundException {
+
 		Optional<Restaurant> found = restaurantRepo.findById(restaurant.getId());
-		
-		if(found.isPresent()) {
+
+		if (found.isPresent()) {
 			Restaurant updated = restaurantRepo.save(restaurant);
-			return ResponseEntity.status(HttpStatus.OK)
-					 .body(updated);
-		}
-		else {
+			return ResponseEntity.status(HttpStatus.OK).body(updated);
+		} else {
 			throw new ResourceNotFoundException("Restaurant with id " + restaurant.getId() + " not found");
 		}
-		
+
 	}
-	
-	//DELETE
-	@DeleteMapping("/restaurants/{restaurant_id}")
-	@ApiOperation(value = "Delete a restaurant by id",
-	  notes = "Delete restaurant",
-	  response = User.class)
-	public ResponseEntity<Restaurant> deleteTodoById(@Valid @PathVariable Long restaurant_id) throws ResourceNotFoundException {
-		
-	    Optional<Restaurant> restaurant = restaurantRepo.findById(restaurant_id);
-	    
-	    if (restaurant.isPresent()) {  
-	    	restaurantRepo.deleteById(restaurant_id);
-	    	return ResponseEntity.status(HttpStatus.OK)
-					 .body(restaurant.get());
-	    }
+
+	// DELETE
+	@DeleteMapping("/restaurants/id/{restaurant_id}")
+	@ApiOperation(value = "Delete a restaurant by id", notes = "Delete restaurant", response = User.class)
+	public ResponseEntity<Restaurant> deleteRestaurantById(@Valid @PathVariable Long restaurant_id)
+			throws ResourceNotFoundException {
+
+		Optional<Restaurant> restaurant = restaurantRepo.findById(restaurant_id);
+
+		if (restaurant.isPresent()) {
+			restaurantRepo.deleteById(restaurant_id);
+			return ResponseEntity.status(HttpStatus.OK).body(restaurant.get());
+		}
 
 		else {
 			throw new ResourceNotFoundException("Restaurant with id " + restaurant_id + " not found");
 		}
 
 	}
-	
-	
+
 }
--- src/test/java/com/cognixia/jump/controller/RestaurantControllerTest.java
+++ src/test/java/com/cognixia/jump/controller/RestaurantControllerTest.java
@@ -4,8 +4,10 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -36,7 +38,7 @@ import com.cognixia.jump.model.User.Role;
 
 /*
  * TODO:
- * 		figure out how to mock delete requests
+ * 		more exception cases
  * 
  */
 
@@ -45,251 +47,260 @@ import com.cognixia.jump.model.User.Role;
 class RestaurantControllerTest {
 
 	private final String STARTING_URI = "http://localhost:8080/api/";
-	
+
 	@Autowired
 	private MockMvc mockMvc;
-	
-	//@MockBean
-	//private CarService service;
-	
-	//@InjectMocks
+
+	// @MockBean
+	// private CarService service;
+
+	// @InjectMocks
 	@MockBean
 	private RestaurantController controller;
-	
+
 	@Test
 	void testGetAllRestaurants_success() throws Exception {
-		
+
 		// set up
 		String uri = STARTING_URI + "restaurants";
-		
+
 		// mock data that will be returned when performing
 		// the get request
 		List<Restaurant> mockRestaurants = Arrays.asList(
-					new Restaurant(1L, "McDonalds", "fast food", "123 address", "5551234567", 3.3, new ArrayList<Review>()),
-					new Restaurant(2L, "Dominos", "pizza", "456 address", "555987654321", 4.5, new ArrayList<Review>())
-				);
-		
+				new Restaurant(1L, "McDonalds", "fast food", "123 address", "5551234567", 3.3, new ArrayList<Review>()),
+				new Restaurant(2L, "Dominos", "pizza", "456 address", "555987654321", 4.5, new ArrayList<Review>()));
+
 		HttpHeaders header = new HttpHeaders();
 		header.setContentType(MediaType.APPLICATION_JSON);
-		
-		ResponseEntity<List<Restaurant>> restaurants = new ResponseEntity<>(
-					mockRestaurants, 
-					header,
-					HttpStatus.OK
-				);
-		
-		
+
+		ResponseEntity<List<Restaurant>> restaurants = new ResponseEntity<>(mockRestaurants, header, HttpStatus.OK);
+
 		// tell Mockito, when the service gets called, return this
-		when( controller.getAllRestaurants()).thenReturn( restaurants );
-		
-		
+		when(controller.getAllRestaurants()).thenReturn(restaurants);
+
 		// execute request
-		mockMvc.perform( get(uri) )
-				.andDo( print() ) // print of the respnse
-				.andExpect( status().isOk() ) // check I have a 200 status code
-				.andExpect( content().contentType(MediaType.APPLICATION_JSON_VALUE) ) // check if json is returned
-				.andExpect( jsonPath("$.length()").value( restaurants.getBody().size() ) ) // checks size of array of objects returned
-				.andExpect( jsonPath("$[0].id").value(restaurants.getBody().get(0).getId()) )
-				.andExpect( jsonPath("$[0].restaurantName").value(restaurants.getBody().get(0).getRestaurantName()) )
-				.andExpect( jsonPath("$[0].description").value(restaurants.getBody().get(0).getDescription()) )
-				.andExpect( jsonPath("$[0].address").value(restaurants.getBody().get(0).getAddress()) )
-				.andExpect( jsonPath("$[0].phoneNumber").value(restaurants.getBody().get(0).getPhoneNumber()) )
-				.andExpect( jsonPath("$[0].rating").value(restaurants.getBody().get(0).getRating()) )
-				.andExpect( jsonPath("$[0].reviews").value(restaurants.getBody().get(0).getReviews()) )
-				
-				.andExpect( jsonPath("$[1].id").value(restaurants.getBody().get(1).getId()) )
-				.andExpect( jsonPath("$[1].restaurantName").value(restaurants.getBody().get(1).getRestaurantName()) )
-				.andExpect( jsonPath("$[1].description").value(restaurants.getBody().get(1).getDescription()) )
-				.andExpect( jsonPath("$[1].address").value(restaurants.getBody().get(1).getAddress()) )
-				.andExpect( jsonPath("$[1].phoneNumber").value(restaurants.getBody().get(1).getPhoneNumber()) )
-				.andExpect( jsonPath("$[1].rating").value(restaurants.getBody().get(1).getRating()) )
-				.andExpect( jsonPath("$[1].reviews").value(restaurants.getBody().get(1).getReviews()) )
-				; 
-		
+		mockMvc.perform(get(uri))
+				//.andDo(print()) // print of the respnse
+				.andExpect(status().isOk()) // check I have a 200 status code
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE)) // check if json is returned
+				.andExpect(jsonPath("$.length()").value(restaurants.getBody().size())) // checks size of array of
+																						// objects returned
+				.andExpect(jsonPath("$[0].id").value(restaurants.getBody().get(0).getId()))
+				.andExpect(jsonPath("$[0].restaurantName").value(restaurants.getBody().get(0).getRestaurantName()))
+				.andExpect(jsonPath("$[0].description").value(restaurants.getBody().get(0).getDescription()))
+				.andExpect(jsonPath("$[0].address").value(restaurants.getBody().get(0).getAddress()))
+				.andExpect(jsonPath("$[0].phoneNumber").value(restaurants.getBody().get(0).getPhoneNumber()))
+				.andExpect(jsonPath("$[0].rating").value(restaurants.getBody().get(0).getRating()))
+				.andExpect(jsonPath("$[0].reviews").value(restaurants.getBody().get(0).getReviews()))
+
+				.andExpect(jsonPath("$[1].id").value(restaurants.getBody().get(1).getId()))
+				.andExpect(jsonPath("$[1].restaurantName").value(restaurants.getBody().get(1).getRestaurantName()))
+				.andExpect(jsonPath("$[1].description").value(restaurants.getBody().get(1).getDescription()))
+				.andExpect(jsonPath("$[1].address").value(restaurants.getBody().get(1).getAddress()))
+				.andExpect(jsonPath("$[1].phoneNumber").value(restaurants.getBody().get(1).getPhoneNumber()))
+				.andExpect(jsonPath("$[1].rating").value(restaurants.getBody().get(1).getRating()))
+				.andExpect(jsonPath("$[1].reviews").value(restaurants.getBody().get(1).getReviews()));
+
 		// verification of execution
-		verify( controller, times(1) ).getAllRestaurants();
+		verify(controller, times(1)).getAllRestaurants();
 		verifyNoMoreInteractions(controller);
-		
+
 	}
-	
+
 	@Test
 	void testGetAllRestaurants_emptyResult() throws Exception {
 		String uri = STARTING_URI + "restaurants";
-		
+
 		List<Restaurant> mockRestaurants = Arrays.asList();
-		
-		HttpHeaders header = new HttpHeaders();
-		header.setContentType(MediaType.APPLICATION_JSON);
-		
-		ResponseEntity<List<Restaurant>> restaurants = new ResponseEntity<>(
-					mockRestaurants, 
-					header,
-					HttpStatus.OK
-				);
-		
-		when( controller.getAllRestaurants()).thenReturn( restaurants );
-		
-		mockMvc.perform( get(uri) )
-				.andDo( print() ) 
-				.andExpect( status().isOk() ) 
-				.andExpect( content().contentType(MediaType.APPLICATION_JSON_VALUE) ) 
-				.andExpect( jsonPath("$.length()").value( restaurants.getBody().size() ) )
-				; 
-		
-		verify( controller, times(1) ).getAllRestaurants();
-		verifyNoMoreInteractions(controller);
-		
-	}
-	
-	
-	@Test
-	void testGetRestaurantById_success() throws Exception{
-		long id = 1;
-		String uri = STARTING_URI + "restaurants/id/{restaurant_id}";
-		
-		Restaurant mockRestaurant = 
-				new Restaurant(1L, "McDonalds", "fast food", "123 address", "5551234567", 3.3, new ArrayList<Review>());
 
 		HttpHeaders header = new HttpHeaders();
 		header.setContentType(MediaType.APPLICATION_JSON);
-		
-		ResponseEntity<Restaurant> restaurant = new ResponseEntity<>(
-					mockRestaurant, 
-					header,
-					HttpStatus.OK
-				);
-		
+
+		ResponseEntity<List<Restaurant>> restaurants = new ResponseEntity<>(mockRestaurants, header, HttpStatus.OK);
+
+		when(controller.getAllRestaurants()).thenReturn(restaurants);
+
+		mockMvc.perform(get(uri))
+		//.andDo(print())
+		.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(jsonPath("$.length()").value(restaurants.getBody().size()));
+
+		verify(controller, times(1)).getAllRestaurants();
+		verifyNoMoreInteractions(controller);
+
+	}
+
+	@Test
+	void testGetRestaurantById_success() throws Exception {
+		long id = 1;
+		String uri = STARTING_URI + "restaurants/id/{restaurant_id}";
+
+		Restaurant mockRestaurant = new Restaurant(1L, "McDonalds", "fast food", "123 address", "5551234567", 3.3,
+				new ArrayList<Review>());
+
+		HttpHeaders header = new HttpHeaders();
+		header.setContentType(MediaType.APPLICATION_JSON);
+
+		ResponseEntity<Restaurant> restaurant = new ResponseEntity<>(mockRestaurant, header, HttpStatus.OK);
+
 		when(controller.getRestaurantById(id)).thenReturn(restaurant);
-		
-		mockMvc.perform( get(uri, id) )
-		.andDo(print())
-		.andExpect( status().isOk() )
-		.andExpect( content().contentType(MediaType.APPLICATION_JSON_VALUE) )
-		.andExpect( jsonPath("$.id").value(restaurant.getBody().getId()) )
-		.andExpect( jsonPath("$.restaurantName").value(restaurant.getBody().getRestaurantName()) )
-		.andExpect( jsonPath("$.description").value(restaurant.getBody().getDescription()) )
-		.andExpect( jsonPath("$.address").value(restaurant.getBody().getAddress()) )
-		.andExpect( jsonPath("$.phoneNumber").value(restaurant.getBody().getPhoneNumber()) )
-		.andExpect( jsonPath("$.rating").value(restaurant.getBody().getRating()) )
-		.andExpect( jsonPath("$.reviews").value(restaurant.getBody().getReviews()) )
-		
+
+		mockMvc.perform(get(uri, id))
+				//.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(jsonPath("$.id").value(restaurant.getBody().getId()))
+				.andExpect(jsonPath("$.restaurantName").value(restaurant.getBody().getRestaurantName()))
+				.andExpect(jsonPath("$.description").value(restaurant.getBody().getDescription()))
+				.andExpect(jsonPath("$.address").value(restaurant.getBody().getAddress()))
+				.andExpect(jsonPath("$.phoneNumber").value(restaurant.getBody().getPhoneNumber()))
+				.andExpect(jsonPath("$.rating").value(restaurant.getBody().getRating()))
+				.andExpect(jsonPath("$.reviews").value(restaurant.getBody().getReviews()))
+
 		;
 
 		verify(controller, times(1)).getRestaurantById(id);
-		verifyNoMoreInteractions(controller);	
-		
+		verifyNoMoreInteractions(controller);
+
 	}
-	
+
 	@Test
-	void testGetRestaurantByName_success() throws Exception{
+	void testGetRestaurantByName_success() throws Exception {
 		String name = "McDonalds";
 		String uri = STARTING_URI + "restaurants/name/{restaurant_name}";
-		
-		Restaurant mockRestaurant = 
-				new Restaurant(1L, "McDonalds", "fast food", "123 address", "5551234567", 3.3, new ArrayList<Review>());
+
+		Restaurant mockRestaurant = new Restaurant(1L, "McDonalds", "fast food", "123 address", "5551234567", 3.3,
+				new ArrayList<Review>());
 
 		HttpHeaders header = new HttpHeaders();
 		header.setContentType(MediaType.APPLICATION_JSON);
-		
-		ResponseEntity<Restaurant> restaurant = new ResponseEntity<>(
-					mockRestaurant, 
-					header,
-					HttpStatus.OK
-				);
-		
+
+		ResponseEntity<Restaurant> restaurant = new ResponseEntity<>(mockRestaurant, header, HttpStatus.OK);
+
 		when(controller.getRestaurantByName(name)).thenReturn(restaurant);
-		
-		mockMvc.perform( get(uri, name) )
-		.andDo(print())
-		.andExpect( status().isOk() )
-		.andExpect( content().contentType(MediaType.APPLICATION_JSON_VALUE) )
-		.andExpect( jsonPath("$.id").value(restaurant.getBody().getId()) )
-		.andExpect( jsonPath("$.restaurantName").value(restaurant.getBody().getRestaurantName()) )
-		.andExpect( jsonPath("$.description").value(restaurant.getBody().getDescription()) )
-		.andExpect( jsonPath("$.address").value(restaurant.getBody().getAddress()) )
-		.andExpect( jsonPath("$.phoneNumber").value(restaurant.getBody().getPhoneNumber()) )
-		.andExpect( jsonPath("$.rating").value(restaurant.getBody().getRating()) )
-		.andExpect( jsonPath("$.reviews").value(restaurant.getBody().getReviews()) )
-		
+
+		mockMvc.perform(get(uri, name))
+				//.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+				.andExpect(jsonPath("$.id").value(restaurant.getBody().getId()))
+				.andExpect(jsonPath("$.restaurantName").value(restaurant.getBody().getRestaurantName()))
+				.andExpect(jsonPath("$.description").value(restaurant.getBody().getDescription()))
+				.andExpect(jsonPath("$.address").value(restaurant.getBody().getAddress()))
+				.andExpect(jsonPath("$.phoneNumber").value(restaurant.getBody().getPhoneNumber()))
+				.andExpect(jsonPath("$.rating").value(restaurant.getBody().getRating()))
+				.andExpect(jsonPath("$.reviews").value(restaurant.getBody().getReviews()))
+
 		;
 
 		verify(controller, times(1)).getRestaurantByName(name);
-		verifyNoMoreInteractions(controller);	
-		
+		verifyNoMoreInteractions(controller);
+
 	}
-	
-	
-	
-	
+
 	@Test
-	void testGetRestaurantById_badId() throws Exception{
+	void testGetRestaurantById_badId() throws Exception {
 		long id = -1;
 		String uri = STARTING_URI + "restaurants/id/{restaurant_id}";
 		when(controller.getRestaurantById(id)).thenThrow(new ResourceNotFoundException(""));
-		
-		mockMvc.perform(get(uri, id))
-		 .andExpect(result -> Assertions.assertTrue(result.getResolvedException() instanceof ResourceNotFoundException));
-		
+
+		mockMvc.perform(get(uri, id)).andExpect(
+				result -> Assertions.assertTrue(result.getResolvedException() instanceof ResourceNotFoundException));
+
 	}
-	
+
 	@Test
-	void testGetRestaurantByName_badName() throws Exception{
+	void testGetRestaurantByName_badName() throws Exception {
 		String name = "hkldfjg";
 		String uri = STARTING_URI + "restaurants/name/{restaurant_name}";
 		when(controller.getRestaurantByName(name)).thenThrow(new ResourceNotFoundException(""));
-		
-		mockMvc.perform(get(uri, name))
-		.andExpect(result -> Assertions.assertTrue(result.getResolvedException() instanceof ResourceNotFoundException));
-		
+
+		mockMvc.perform(get(uri, name)).andExpect(
+				result -> Assertions.assertTrue(result.getResolvedException() instanceof ResourceNotFoundException));
+
 	}
-	
-	/*
+
 	@Test
-	void testAddUser_success() throws Exception{
-		String uri = STARTING_URI + "users";
-		String json = 
-				"{\n"
-				+ " \"id\": -1,\n"
-				+ " \"username\": \"usernametest\",\n"
-				+ " \"password\": \"passwordtest\",\n"
-				+ " \"role\": \"ROLE_USER\",\n"
-				+ " \"reviews\": []\n"
-				+ "}";
-		
-		User mockUser = new User(1l, "usernametest", "passwordtest", Role.ROLE_USER, new ArrayList<Review>());
+	void testAddRestaurant_success() throws Exception {
+		String uri = STARTING_URI + "restaurants";
+		String json = "{\n" + " \"id\": -1,\n" + " \"restaurantName\": \"McDonalds\",\n"
+				+ " \"description\": \"fast food\",\n" + " \"address\": \"1011 S Lake Dr, Lexington SC, 29072\",\n"
+				+ " \"phoneNumber\": \"8039578808\",\n" + " \"rating\": 4.2,\n" + " \"reviews\": []\n" + "}";
+
+		Restaurant mockRestaurant = new Restaurant(-1L, "McDonalds", "fast food", "1011 S Lake Dr, Lexington SC, 29072",
+				"8039578808", 4.0, new ArrayList<Review>());
+
 		HttpHeaders header = new HttpHeaders();
 		header.setContentType(MediaType.APPLICATION_JSON);
-		
-		ResponseEntity<User> user = new ResponseEntity<>(
-					mockUser, 
-					header,
-					HttpStatus.CREATED
-				);
-		
-		when(controller.addUser(Mockito.any(User.class))).thenReturn(user);
-		
-		mockMvc.perform(post(uri)
-				.contentType(MediaType.APPLICATION_JSON)
-				.content(json))
-			.andDo(print())
-			.andExpect(status().isCreated())
-			.andExpect( jsonPath("$.id").value(user.getBody().getId()) )
-			.andExpect( jsonPath("$.username").value(user.getBody().getUsername()) )
-			.andExpect( jsonPath("$.password").value(user.getBody().getPassword()) )
-			.andExpect( jsonPath("$.role").value(user.getBody().getRole().toString()) )
-			.andExpect( jsonPath("$.reviews").value(user.getBody().getReviews()) )
-		;
+
+		ResponseEntity<Restaurant> restaurant = new ResponseEntity<>(mockRestaurant, header, HttpStatus.CREATED);
+
+		when(controller.addRestaurant(Mockito.any(Restaurant.class))).thenReturn(restaurant);
+
+		mockMvc.perform(post(uri).contentType(MediaType.APPLICATION_JSON).content(json))
+		//.andDo(print())
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.id").value(restaurant.getBody().getId()))
+				.andExpect(jsonPath("$.restaurantName").value(restaurant.getBody().getRestaurantName()))
+				.andExpect(jsonPath("$.description").value(restaurant.getBody().getDescription()))
+				.andExpect(jsonPath("$.address").value(restaurant.getBody().getAddress()))
+				.andExpect(jsonPath("$.phoneNumber").value(restaurant.getBody().getPhoneNumber()))
+				.andExpect(jsonPath("$.rating").value(restaurant.getBody().getRating()))
+				.andExpect(jsonPath("$.reviews").value(restaurant.getBody().getReviews()));
 	}
-	*/
+
+	@Test
+	void deleteRestaurant_success() throws Exception {
+		long id = 1;
+	
+		String uri = STARTING_URI + "restaurants/id/{restaurant_id}";
+		
+
+		Restaurant mockRestaurant = new Restaurant(id, "McDonalds", "fast food", "1011 S Lake Dr, Lexington SC, 29072",
+				"8039578808", 4.0, new ArrayList<Review>());
+
+		HttpHeaders header = new HttpHeaders();
+		header.setContentType(MediaType.APPLICATION_JSON);
+
+		ResponseEntity<Restaurant> restaurant = new ResponseEntity<>(mockRestaurant, header, HttpStatus.CREATED);
+
+		when(controller.addRestaurant(Mockito.any(Restaurant.class))).thenReturn(restaurant);
+		
+
+		mockMvc.perform(delete(uri, id))
+		//.andDo(print())
+				.andExpect(status().isOk());
+		
+		Mockito.verify(controller, times(1)).deleteRestaurantById(id);
+	}
+	
+	@Test
+	void testEditRestaurant_success() throws Exception {
+		String uri = STARTING_URI + "update/restaurants";
+		String json = "{\n" + " \"id\": -1,\n" + " \"restaurantName\": \"McDonalds\",\n"
+				+ " \"description\": \"fast food\",\n" + " \"address\": \"1011 S Lake Dr, Lexington SC, 29072\",\n"
+				+ " \"phoneNumber\": \"8039578808\",\n" + " \"rating\": 4.2,\n" + " \"reviews\": []\n" + "}";
+
+		Restaurant mockRestaurant = new Restaurant(1L, "McDonalds", "fast food", "1011 S Lake Dr, Lexington SC, 29072",
+				"8039578808", 4.0, new ArrayList<Review>());
+
+		HttpHeaders header = new HttpHeaders();
+		header.setContentType(MediaType.APPLICATION_JSON);
+
+		ResponseEntity<Restaurant> restaurant = new ResponseEntity<>(mockRestaurant, header, HttpStatus.OK);
+
+		when(controller.updateRestaurant(Mockito.any(Restaurant.class))).thenReturn(restaurant);
+
+		mockMvc.perform(put(uri).contentType(MediaType.APPLICATION_JSON).content(json))
+		.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.id").value(restaurant.getBody().getId()))
+				.andExpect(jsonPath("$.restaurantName").value(restaurant.getBody().getRestaurantName()))
+				.andExpect(jsonPath("$.description").value(restaurant.getBody().getDescription()))
+				.andExpect(jsonPath("$.address").value(restaurant.getBody().getAddress()))
+				.andExpect(jsonPath("$.phoneNumber").value(restaurant.getBody().getPhoneNumber()))
+				.andExpect(jsonPath("$.rating").value(restaurant.getBody().getRating()))
+				.andExpect(jsonPath("$.reviews").value(restaurant.getBody().getReviews()));
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
